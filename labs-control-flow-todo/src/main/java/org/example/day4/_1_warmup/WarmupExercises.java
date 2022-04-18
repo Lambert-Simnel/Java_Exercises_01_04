bc30a2957f11842d6bd8ebbced618219753182f3
Lambert-Simnel <Josephsut@gmail.com>
2022-04-18 16:27:41 +0100

    Exercises from Thursday

---
+++ labs-control-flow-todo/src/main/java/org/example/day4/_1_warmup/WarmupExercises.java
@@ -0,0 +1,20 @@
+package org.example.day4._1_warmup;
+
+public class WarmupExercises {
+
+
+
+    boolean isFirstGreaterThanSecond(int n1, int n2){
+
+        if (n1 > n2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean equalString(String s1, String s2){
+        return s1.equals(s2);
+    }
+
+}
